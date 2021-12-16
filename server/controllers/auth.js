20b190ce69f6a6626f55681e299931c6814d028d
Poleszak <poleszak@protonmail.com>
2021-12-16 23:57:22 +0100

    Server-Side Auth

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -30,7 +30,23 @@ const signup = async (req, res) => {
 const login = async (req, res) => {
     try {
         const { username, password } = req.body;
+
+        const serverClient = connect(api_key, api_secret, app_id);
+        const client = StreamChat.getInstance(api_key, api_secret);
         
+        const { users } = await client.queryUsers({ name: username });
+
+        if(!users.length) return res.status(400).json({ message: 'User not found' })
+
+        const succes = await bcrypt.compare(password, users[0].hashedPassword);
+
+        const token = serverClient.createUserToken(users[0].id);
+
+        if(success) {
+            res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id });
+        } else {
+            res.status(500).json({ message: 'Incorrect password' })
+        }
     } catch (error) {
         console.log(error)
 
