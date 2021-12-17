a73155526804a0e3193f1a70fb409e2fbd282ccb
Poleszak <poleszak@protonmail.com>
2021-12-17 21:46:55 +0100

    Client-Server Communication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,6 +3,8 @@ const bcrypt = require('bcrypt');
 const StreamChat = require('stream-chat');
 const crypto = require('crypto');
 
+require('dotenv').config();
+
 const api_key = process.env.STREAM_API_KEY;
 const api_secret = process.env.STREAM_API_SECRET;
 const api_id = process.env.STREAM_APP_ID;
