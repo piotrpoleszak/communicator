e5e20cb72c39bce409c1dac1699826ccf5a28eef
Poleszak <poleszak@protonmail.com>
2021-12-18 23:49:16 +0100

    ChannelListContainer - Channels

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { ChannelList, useChatContext } from "stream-chat-react";
-import Cookies from "universal-cookie/es6";
+import React, { useState } from 'react';
+import { ChannelList, useChatContext } from 'stream-chat-react';
+import Cookies from 'universal-cookie';
 
-import { ChannelSearch, TeamChannelList, TeamChannelPreview } from "./";
-import HospitalIcon from '../assets/hospital.png';
-import LogoutIcon from '../assets/logout.png';
+import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
 
 const cookies = new Cookies();
 
@@ -15,7 +15,6 @@ const SideBar = ({ logout }) => (
                 <img src={HospitalIcon} alt="Hospital" width="30" />
             </div>
         </div>
-
         <div className="channel-list__sidebar__icon2">
             <div className="icon1__inner" onClick={logout}>
                 <img src={LogoutIcon} alt="Logout" width="30" />
@@ -26,11 +25,21 @@ const SideBar = ({ logout }) => (
 
 const CompanyHeader = () => (
     <div className="channel-list__header">
-        <p className="channel-list__header__text">Chat</p>
+        <p className="channel-list__header__text">Medical Pager</p>
     </div>
 )
 
-const ChannelListContainer = () => {
+const customChannelTeamFilter = (channels) => {
+    return channels.filter((channel) => channel.type === 'team');
+}
+
+const customChannelMessagingFilter = (channels) => {
+    return channels.filter((channel) => channel.type === 'messaging');
+}
+
+const ChannelListContent = ({ isCreating, setIsCreating, setCreateType, setIsEditing, setToggleContainer }) => {
+    const { client } = useChatContext();
+
     const logout = () => {
         cookies.remove("token");
         cookies.remove('userId');
@@ -43,47 +52,95 @@ const ChannelListContainer = () => {
         window.location.reload();
     }
 
+    const filters = { members: { $in: [client.userID] } };
+
     return (
         <>
-            <SideBar logout={logout}/>  
+            <SideBar logout={logout} />
             <div className="channel-list__list__wrapper">
                 <CompanyHeader />
-                <ChannelSearch />
+                <ChannelSearch setToggleContainer={setToggleContainer} />
                 <ChannelList 
-                    filters={{}}
-                    channelRenderFilterFn={() => {}}
+                    filters={filters}
+                    channelRenderFilterFn={customChannelTeamFilter}
                     List={(listProps) => (
                         <TeamChannelList 
                             {...listProps}
                             type="team"
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType} 
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
                         />
                     )}
                     Preview={(previewProps) => (
                         <TeamChannelPreview 
                             {...previewProps}
+                            setIsCreating={setIsCreating}
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
                             type="team"
                         />
                     )}
                 />
                 <ChannelList 
-                    filters={{}}
-                    channelRenderFilterFn={() => {}}
+                    filters={filters}
+                    channelRenderFilterFn={customChannelMessagingFilter}
                     List={(listProps) => (
                         <TeamChannelList 
                             {...listProps}
                             type="messaging"
+                            isCreating={isCreating}
+                            setIsCreating={setIsCreating}
+                            setCreateType={setCreateType} 
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
                         />
                     )}
                     Preview={(previewProps) => (
                         <TeamChannelPreview 
                             {...previewProps}
-                            type="team"
+                            setIsCreating={setIsCreating}
+                            setIsEditing={setIsEditing}
+                            setToggleContainer={setToggleContainer}
+                            type="messaging"
                         />
                     )}
                 />
             </div>
         </>
     );
+}
+
+const ChannelListContainer = ({ setCreateType, setIsCreating, setIsEditing }) => {
+    const [toggleContainer, setToggleContainer] = useState(false);
+
+    return (
+        <>
+            <div className="channel-list__container">
+              <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing} 
+              />
+            </div>
+
+            <div className="channel-list__container-responsive"
+                style={{ left: toggleContainer ? "0%" : "-89%", backgroundColor: "#005fff"}}
+            >
+                <div className="channel-list__container-toggle" onClick={() => setToggleContainer((prevToggleContainer) => !prevToggleContainer)}>
+                </div>
+                <ChannelListContent 
+                setIsCreating={setIsCreating} 
+                setCreateType={setCreateType} 
+                setIsEditing={setIsEditing}
+                setToggleContainer={setToggleContainer}
+              />
+            </div>
+        </>
+    )
+
 }
 
 export default ChannelListContainer;
